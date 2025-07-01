869687b0d34fc1ce79782d663039c4bc9041ff3a
ggarabs <ggarabetti0607@gmail.com>
2025-07-01 18:09:37 -0300

    project started

---
+++ src/main/java/com/simplebuy/simplebuy_back/SimplebuyBackApplication.java
@@ -0,0 +1,13 @@
+package com.simplebuy.simplebuy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebuyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebuyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplebuy/simplebuy_back/SimplebuyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplebuy.simplebuy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebuyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
